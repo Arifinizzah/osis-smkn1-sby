c7fe3a33991237d604078d52a64222b79c0efadd
arifinizz <arifin.izzah15@gmail.com>
2018-03-07 06:05:55 +0700

    adding troll page:v

---
+++ admin/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1.0, user-scalable=no">
+    <title>CARI APA???</title>
+    <style>
+      body{
+        margin: 0;
+        display: block;
+        background: url(http://images.digopaul.com/wp-content/uploads/related_images/2015/09/10/gore_2.jpg);
+        background-repeat: no-repeat;
+        background-size: cover;
+      }
+    </style>
+  </head>
+  <body>
+    <center><h1>CARI APA???</h1></center>
+  </body>
+</html>
